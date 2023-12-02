3bed50e56cad8472c57f28059d8c171ac929325b
milind64 <milindmurmu@gmail.com>
2023-12-02 15:27:26 +0530

    firebase working

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:temp1/auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final User? user = Auth().currentUser;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget _userUid() {
+    return Text(user?.email ?? 'User email');
+  }
+
+  Widget _signOutBtn() {
+    return ElevatedButton(
+      onPressed: signOut,
+      child: const Text('Sign Out'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AlumNet'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[_userUid(), _signOutBtn()]),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -11,201 +11,214 @@ class _SignUpState extends State<SignUp> {
   TextEditingController _nameController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  final FocusNode _collegeIdFocus = FocusNode();
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       resizeToAvoidBottomInset: false,
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 0, 0, 0),
-              Color.fromARGB(255, 36, 2, 98),
-              Color.fromARGB(235, 59, 8, 96),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
         child: Container(
-          padding: EdgeInsets.all(8.0),
-          margin: EdgeInsets.symmetric(vertical: 110, horizontal: 25),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.2),
-            borderRadius: const BorderRadius.all(Radius.circular(30)),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 36, 2, 98),
+                Color.fromARGB(235, 59, 8, 96),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.symmetric(vertical: 110, horizontal: 25),
+              decoration: BoxDecoration(
+                color:
+                    const Color.fromARGB(255, 255, 255, 255).withOpacity(0.2),
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
               ),
-              Text(
-                'Sign Up',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  controller: _collegeIdController,
-                  decoration: InputDecoration(
-                    labelText: 'College ID',
-                    hintText: 'Enter your valid college id',
-                    hintStyle:
-                        TextStyle(color: Colors.white), // Set hint text color
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.white), // Set border color when focused
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .white), // Set border color when not focused
-                    ),
-                    labelStyle:
-                        TextStyle(color: Colors.white), // Set label text color
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                  style: TextStyle(color: Colors.white), // Set input text color
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    hintText: 'Enter your full name',
-                    hintStyle:
-                        TextStyle(color: Colors.white), // Set hint text color
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.white), // Set border color when focused
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .white), // Set border color when not focused
-                    ),
-                    labelStyle:
-                        TextStyle(color: Colors.white), // Set label text color
                   ),
-                  style: TextStyle(color: Colors.white), // Set input text color
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter valid mail id',
-                    hintStyle:
-                        TextStyle(color: Colors.white), // Set hint text color
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.white), // Set border color when focused
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .white), // Set border color when not focused
-                    ),
-                    labelStyle:
-                        TextStyle(color: Colors.white), // Set label text color
+                  SizedBox(
+                    height: 20,
                   ),
-                  style: TextStyle(color: Colors.white), // Set input text color
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  style: TextStyle(color: Colors.white), // Set input text color
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your secure password',
-                    hintStyle:
-                        TextStyle(color: Colors.white), // Set hint text color
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.white), // Set border color when focused
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors
-                              .white), // Set border color when not focused
-                    ),
-                    labelStyle:
-                        TextStyle(color: Colors.white), // Set label text color
-                  ),
-                ),
-              ),
-              SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Perform sign-in logic here
-                  String college = _collegeIdController.text;
-                  String name = _nameController.text;
-                  String email = _emailController.text;
-                  String password = _passwordController.text;
-                  // Add your authentication logic here
-                  // For a simple example, just print the email and password
-                  print('CollegeId: $college');
-                  print('Name: $name');
-                  print('Email: $email');
-                  print('Password: $password');
-                },
-                child: Text('Sign Up'),
-              ),
-              SizedBox(height: 32.0),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to sign-up page
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignIn()),
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Already a member? ',
-                    style: TextStyle(color: Colors.white),
-                    children: [
-                      TextSpan(
-                        text: 'Sign in',
-                        style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      focusNode: _collegeIdFocus,
+                      controller: _collegeIdController,
+                      decoration: InputDecoration(
+                        labelText: 'College ID',
+                        hintText: 'Enter your valid college id',
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
                         ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        labelStyle: TextStyle(color: Colors.white),
                       ),
-                    ],
+                      style: TextStyle(color: Colors.white),
+                      onTap: () {
+                        _scrollToCenter(_collegeIdFocus);
+                      },
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      focusNode: _nameFocus,
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        hintText: 'Enter your full name',
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      onTap: () {
+                        _scrollToCenter(_nameFocus);
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      focusNode: _emailFocus,
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter valid mail id',
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      onTap: () {
+                        _scrollToCenter(_emailFocus);
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      focusNode: _passwordFocus,
+                      obscureText: true,
+                      controller: _passwordController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your secure password',
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                      onTap: () {
+                        _scrollToCenter(_passwordFocus);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      String college = _collegeIdController.text;
+                      String name = _nameController.text;
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
+                      print('CollegeId: $college');
+                      print('Name: $name');
+                      print('Email: $email');
+                      print('Password: $password');
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                  SizedBox(height: 32.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignIn()),
+                      );
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Already a member? ',
+                        style: TextStyle(color: Colors.white),
+                        children: [
+                          TextSpan(
+                            text: 'Sign in',
+                            style: TextStyle(
+                              color: Colors.white,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  void _scrollToCenter(FocusNode focusNode) {
+    Scrollable.ensureVisible(
+      focusNode.context!,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
     );
   }
 }
